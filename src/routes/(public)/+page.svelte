088296ce6e522ecb1a8898f34002156d8e8e774a
kursten putman <fglad@gmail.com>
2023-12-13 11:22:32 -0600

    Add buttons.

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,10 +1,10 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center gap-6">
-    <p class="text-[2.6rem] leading-[0.9] font-semibold"><span class="text-brand">Quado</span><br />devivery made easy</p>
-    <p class="text-lg text-primary-700 max-w-[80%] lg:max-w-[47%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sed, eum aliquid error assumenda optio illum distinctio at dignissimos!</p>
+    <p class="text-[2.8rem] leading-[0.9] font-semibold"><span class="text-brand">Quado</span><br />devivery made easy</p>
+    <p class="text-lg text-primary-700 max-w-[80%] lg:max-w-[41%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sed, eum aliquid error assumenda optio illum distinctio at dignissimos!</p>
 
-    <div class="flex gap-6">
-        <button>Register</button>
-        <button>Learn more</button>
+    <div class="flex gap-6 text-base">
+        <button class="glass">Register</button>
+        <button class="glass">Learn more</button>
     </div>
 </div>
 
