8e1ba31d4101cba0c8be88cc197829f9fda7bb68
kursten putman <fglad@gmail.com>
2023-12-13 11:25:05 -0600

    fixed spelling

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,5 +1,5 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center gap-6">
-    <p class="text-[2.8rem] leading-[0.9] font-semibold"><span class="text-brand">Quado</span><br />devivery made easy</p>
+    <p class="text-[2.8rem] leading-[0.9] font-semibold"><span class="text-brand">Quado</span><br />delivery made easy</p>
     <p class="text-lg text-primary-700 max-w-[80%] lg:max-w-[41%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sed, eum aliquid error assumenda optio illum distinctio at dignissimos!</p>
 
     <div class="flex gap-6 text-base">
