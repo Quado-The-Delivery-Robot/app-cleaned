bcfc2b3ab811defa6072a72fde197db9fd7ac4f7
kursten putman <fglad@gmail.com>
2024-01-05 11:26:25 -0600

    worked on home page

--- src/routes/(protected)/home/+page.svelte
+++ src/routes/(protected)/home/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+    import { page } from "$app/stores";
     import Search from "$lib/protected/home/search.svelte";
+
+    export let data: any;
 </script>
 
 <Search />
+
+{#each Object.entries(data.page) as [sectionName, sectionData]}
+    dnsbf
+{/each}
+<div class=" flex flex-row py-2 gap-2 w-full">
+    <button class="glass rounded-lg py-2 px-4">{data.image}</button>
+    <button class="glass rounded-lg py-2 px-4">{data.image}</button>
+</div>
