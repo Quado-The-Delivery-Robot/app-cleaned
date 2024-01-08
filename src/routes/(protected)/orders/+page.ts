3211e1fe039c9ca8805d029cecedb6f47c883365
Landon Redmond <name1234561.po@gmail.com>
2024-01-08 11:06:31 -0600

    Add orders page.

---
+++ src/routes/(protected)/orders/+page.ts
@@ -0,0 +1,10 @@
+import { PUBLIC_ENDPOINT } from "$env/static/public";
+import type { order } from "$lib/core/types";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({ fetch }) => {
+    const result = await fetch(`${PUBLIC_ENDPOINT}/v1/orders/get`);
+    const { orders }: { success: boolean; orders: order[] } = await result.json();
+
+    return { orders };
+};
