98436778ffd55ccf3794efa185762fac78a55053
Landon Redmond <name1234561.po@gmail.com>
2024-01-09 10:34:11 -0600

    Add demo cart page.

---
+++ src/routes/(protected)/cart/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { PUBLIC_ENDPOINT } from "$env/static/public";
+
+    let result: boolean = false;
+
+    async function send() {
+        const createFetch = await fetch(`${PUBLIC_ENDPOINT}/v1/orders/new`, {
+            method: "POST",
+            body: JSON.stringify({
+                restaurant: "subway",
+                items: {
+                    id: 0,
+                    quantity: 1,
+                },
+            }),
+        });
+        const { success } = await createFetch.json();
+        result = success;
+    }
+</script>
+
+<button on:click={send}>place order</button>
+{result}
