8e8163d981e4307bfae1bfca7ba0aa3bccab9340
Landon Redmond <name1234561.po@gmail.com>
2024-01-10 11:13:37 -0600

    Pass headers.

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -5,10 +5,11 @@ async function redirect({ request, url, fetch }: RequestEvent): Promise<Response
     const apiURL: string = url.searchParams.get("url")!;
     const method: string = url.searchParams.get("method") || "GET";
     const body: string | null = url.searchParams.get("body");
-    console.log(body);
+
     return json(
         await fetch(`${PUBLIC_ENDPOINT}/${apiURL}`, {
             body: body,
+            headers: request.headers,
             method: method || "GET",
             credentials: "include",
         }).then(async (response) => await response.json())
