94d46a7213ce6185874a276115264e320e9c688b
Landon Redmond <name1234561.po@gmail.com>
2024-01-11 11:04:02 -0600

    Fix NODE_ENV.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "@auth/core/providers/google";
-import { GOOGLE_CLIENT_ID, GOOGLE_SECERT } from "$env/static/private";
+import { GOOGLE_CLIENT_ID, GOOGLE_SECERT, NODE_ENV } from "$env/static/private";
 import database from "$lib/core/database";
 import type { Handle } from "@sveltejs/kit";
 
@@ -18,7 +18,7 @@ export const handle: Handle = SvelteKitAuth({
                 httpOnly: true,
                 sameSite: "lax",
                 path: "/",
-                secure: process.env.NODE_ENV === "production" ? true : false,
+                secure: NODE_ENV === "production" ? true : false,
             },
         },
     },
