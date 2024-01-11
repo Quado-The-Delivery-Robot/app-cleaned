a9e39d2daa00c3a0a986a33a20d1e3a306cd20e0
kursten putman <fglad@gmail.com>
2024-01-11 11:29:25 -0600

    worked on home page

--- src/lib/protected/components/section.svelte
+++ src/lib/protected/components/section.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+    import type { restuarantItem, restaurant } from "$lib/core/types";
+
+    export let data: restaurant[] | restuarantItem[] = [];
+    export let sectionData: string = "";
+    export let type: string;
+    export let sectionName: string;
+</script>
+
 <div class=" flex felx-row gap-2 py-2 px-4">
     <button>tester</button>
 </div>
