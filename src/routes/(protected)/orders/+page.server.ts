d86d56b45e959117d26f1c4d70e8cf4f5f800200
Landon Redmond <name1234561.po@gmail.com>
2024-02-01 10:28:23 -0600

    Remove unused type & change to server load.

--- src/routes/(protected)/orders/+page.server.ts
+++ src/routes/(protected)/orders/+page.server.ts
@@ -1,8 +1,7 @@
 import { PUBLIC_ENDPOINT } from "$env/static/public";
-import type { order } from "$lib/core/types";
-import type { PageLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
     const result = await fetch(`${PUBLIC_ENDPOINT}/v1/orders/get`, {
         credentials: "include",
     });
