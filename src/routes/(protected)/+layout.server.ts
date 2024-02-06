1165473c89a799a6835b824c774867d15c102c29
Landon Redmond <name1234561.po@gmail.com>
2024-02-06 11:05:07 -0600

    Disable ssr.

--- src/routes/(protected)/+layout.server.ts
+++ src/routes/(protected)/+layout.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
+export const ssr = false;
+
 export const load: LayoutServerLoad = async ({ locals }) => {
     const session = await locals.getSession();
 
