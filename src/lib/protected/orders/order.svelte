72b9a5e091230e3420385dc26996303eb8bb5975
Landon Redmond <name1234561.po@gmail.com>
2024-02-07 10:13:13 -0600

    Improving component.

--- src/lib/protected/orders/order.svelte
+++ src/lib/protected/orders/order.svelte
@@ -5,10 +5,10 @@
 <div class="glass p-5 text-left">
     <div class="flex justify-between items-center gap-2">
         <p class="text-2xl text-left font-semibold">{order.restaurant}</p>
-        <p class=" text-red-400 font-medium text-lg">${order.price}</p>
+        <p class="text-red-400 font-medium text-lg">${order.price}</p>
     </div>
 
-    <p class="text-primary font-medium text-lg flex justify-start items-center mb-2">
+    <p class="font-medium text-primary-700 text-lg flex justify-start items-center mb-2">
         {#if order.state !== "Delivered" && order.state !== "Not started"}
             <div class="inProgressIndicator aspect-square h-2.5 bg-brand rounded-full mr-2" />
         {/if}
