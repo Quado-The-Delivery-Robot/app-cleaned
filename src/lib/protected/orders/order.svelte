dc5dacb2e0846b79c21bf535c33eea1c195bb090
Landon Redmond <name1234561.po@gmail.com>
2024-02-07 10:09:14 -0600

    Improve order component styling.

--- src/lib/protected/orders/order.svelte
+++ src/lib/protected/orders/order.svelte
@@ -2,7 +2,7 @@
     export let order: any;
 </script>
 
-<div class="rounded-lg p-5 bg-backgroundSecondary text-left">
+<div class="glass p-5 text-left">
     <div class="flex justify-between items-center gap-2">
         <p class="text-2xl text-left font-semibold">{order.restaurant}</p>
         <p class=" text-red-400 font-medium text-lg">${order.price}</p>
