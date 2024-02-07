e5ffddc4854f25316a10425c1971a2b369e652d0
kursten putman <fglad@gmail.com>
2024-02-07 11:20:39 -0600

    Work on item containers.

--- src/lib/protected/components/itemContainers/icon.svelte
+++ src/lib/protected/components/itemContainers/icon.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+    import { SplideSlide } from "@splidejs/svelte-splide";
     import type { sectionItem } from "$lib/core/types";
 
     export let data: sectionItem;
 </script>
 
-<button class="w-12 aspect-square">
-    <img class="w-full h-full p-2" src={data.image} alt={data.name} />
-</button>
+<SplideSlide>
+    <button class="w-12 aspect-square">
+        <img class="w-full h-full p-2" src={data.image} alt={data.name} />
+    </button>
+</SplideSlide>
---
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { SplideSlide } from "@splidejs/svelte-splide";
+    import type { sectionItem } from "$lib/core/types";
+
+    export let data: sectionItem;
+</script>
+
+<SplideSlide>
+    <button class="gap-2 w-12 rounded-lg">
+        <div class=" p-2 flex items-center text-left justify-start snap-start">
+            <p class="text-lg font-semibold">{data.name}</p>
+            <p class="text-sm">{data.description}</p>
+        </div>
+    </button>
+</SplideSlide>
