cc6cd1145d9ed037e5a2308818240c47898bc032
kursten putman <fglad@gmail.com>
2024-02-08 10:59:46 -0600

    worked on item conainers

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import { SplideSlide } from "@splidejs/svelte-splide";
-    import type { sectionItem } from "$lib/core/types";
+    import type { sectionItem, restuarantItem } from "$lib/core/types";
 
     export let data: sectionItem;
+    export let itemData: restuarantItem;
 </script>
 
 <SplideSlide>
     <button class="gap-2 w-12 rounded-lg">
         <div class=" p-2 flex items-center text-left justify-start snap-start">
-            <p class="text-lg font-semibold">{data.name}</p>
-            <p class="text-sm">{data.description}</p>
+            <p class="text-lg font-bold">{data.name}</p>
+            <p></p>
+            <p class="text-sm">{itemData.description}</p>
         </div>
     </button>
 </SplideSlide>
