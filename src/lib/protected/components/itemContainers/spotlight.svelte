e2bdcbf68c5ec7c05c02feb348f0998a7b3e8b5a
kursten putman <fglad@gmail.com>
2024-02-08 11:02:22 -0600

    added item name and price

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -10,7 +10,7 @@
     <button class="gap-2 w-12 rounded-lg">
         <div class=" p-2 flex items-center text-left justify-start snap-start">
             <p class="text-lg font-bold">{data.name}</p>
-            <p></p>
+            <p class="text-base"><span>{itemData.name}</span>{itemData.price}</p>
             <p class="text-sm">{itemData.description}</p>
         </div>
     </button>
