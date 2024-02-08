89065fbb651a43d655b2e5bfbcb4d8990dc31fa7
Landon Redmond <name1234561.po@gmail.com>
2024-02-08 11:01:47 -0600

    Edit base container & change section containers to use base container.

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import { SplideSlide } from "@splidejs/svelte-splide";
+    import Base from "./base.svelte";
     import type { sectionItem, restuarantItem } from "$lib/core/types";
 
     export let data: sectionItem;
     export let itemData: restuarantItem;
 </script>
 
-<SplideSlide>
+<Base>
     <button class="gap-2 w-12 rounded-lg">
         <div class=" p-2 flex items-center text-left justify-start snap-start">
             <p class="text-lg font-bold">{data.name}</p>
@@ -14,4 +14,4 @@
             <p class="text-sm">{itemData.description}</p>
         </div>
     </button>
-</SplideSlide>
+</Base>
