1ae476fec5b4927a0d989a06d6243441878ed076
Landon Redmond <name1234561.po@gmail.com>
2024-02-14 10:26:46 -0600

    Create basic restaurant page.

---
+++ src/routes/(public)/restaurant/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Title from "$lib/core/components/title.svelte";
+    import { onMount } from "svelte";
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+    let restaurantName: string = "Restaurant";
+
+    onMount(() => {
+        restaurantName = data.restaurant.name;
+    });
+</script>
+
+<Title>{restaurantName}</Title>
+
+{#await data.restaurant}
+    <p>Loading...</p>
+{:then}
+    <p>{data.restaurant.description}</p>
+{/await}
