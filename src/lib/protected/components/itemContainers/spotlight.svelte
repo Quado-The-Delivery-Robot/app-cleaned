6fdcd478c9e089979f455c4bbb39c6ac6163e3e6
kursten putman <fglad@gmail.com>
2024-02-14 10:38:55 -0600

    increased spotlight size

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <Base>
-    <button class="gap-2 w-12 rounded-lg bg-white">
-        <div class=" h-full p-2 flex items-center text-left justify-start snap-start overflow-hidden">
+    <button class="gap-2 w-24 rounded-lg bg-white">
+        <div class=" p-2 flex items-center text-left justify-start snap-start overflow-hidden">
             <p class="text-base"><span>{data.name}</span>{data.data.subName}</p>
             <p class="text-sm">{data.data.description}</p>
         </div>
