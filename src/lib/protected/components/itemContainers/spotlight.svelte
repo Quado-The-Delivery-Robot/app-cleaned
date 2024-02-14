0577cb15237ab53668941031677677845a5f1e7f
kursten putman <fglad@gmail.com>
2024-02-14 10:41:45 -0600

    changed stoplight size

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <Base>
-    <button class="gap-2 w-24 rounded-lg bg-white">
+    <button class="gap-2 f-fit w-fit rounded-lg bg-white">
         <div class=" p-2 flex items-center text-left justify-start snap-start overflow-hidden">
             <p class="text-base"><span>{data.name}</span>{data.data.subName}</p>
             <p class="text-sm">{data.data.description}</p>
