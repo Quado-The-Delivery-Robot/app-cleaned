5138aa693879f1fe62d1bc02889a476fa91870e7
kursten putman <fglad@gmail.com>
2024-02-15 11:28:37 -0600

    imported section to restuarant page

--- src/routes/(app)/(public)/restaurant/[slug]/+page.svelte
+++ src/routes/(app)/(public)/restaurant/[slug]/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
     import Title from "$lib/core/components/title.svelte";
-    import { Splide } from "@splidejs/svelte-splide";
+    import Section from "$lib/protected/components/section.svelte";
     import { onMount } from "svelte";
     import type { PageData } from "./$types";
 
     export let data: PageData;
-    export let hasHeader: boolean;
-    export let name: string;
     let restaurantName: string = "Restaurant";
 
     onMount(() => {
@@ -21,16 +19,3 @@
 {:then}
     <p>{data.restaurant.description}</p>
 {/await}
-
-<div class="w-full">
-    {#if hasHeader}
-        <div class="flex justify-between items-center mb-1">
-            <p class="text-lg font-semibold">{name}</p>
-            <a href="/{name.toLowerCase()}/all" class="flex items-center text-sm opacity-50 font-light">See all </a>
-        </div>
-    {/if}
-
-    <div class="rounded-lg overflow-hidden">
-        <Splide></Splide>
-    </div>
-</div>
