1cb3ee534c4b6c7347bf7cf9c905f35c8c6793e8
Landon Redmond <name1234561.po@gmail.com>
2024-02-15 11:21:40 -0600

    Limit string length.

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -13,13 +13,21 @@
             backgroundColor = (data as any).colors[0];
             borderColor = shadeColor(backgroundColor, -50);
         }
+
+        if (data.data.name.length > 37) {
+            data.data.name.value = `${data.data.name.value.slice(0, 37)}...`;
+        }
+
+        if (data.data.description.length > 47) {
+            data.data.description.value = `${data.data.description.value.slice(0, 37)}...`;
+        }
     });
 </script>
 
 <Base>
     <button class="gap-2 aspect-square w-48 glass text-center flex items-center justify-center flex-col font-semibold" style="background-color: {backgroundColor} / var(--tw-bg-opacity); border-color: {borderColor};">
         <img class="w-full aspect-square" src={data.image} alt={data.name} />
-        <p>{data.name}</p>
+        <p class="font medium">{data.name}</p>
         <p>{data.data.subName}</p>
         <p class="text-sm font-normal">{data.data.description}</p>
     </button>
