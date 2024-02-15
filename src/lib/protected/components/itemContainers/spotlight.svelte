ebdde3dcda1a18235ea8d6b8a851a61f1feeb0bc
kursten putman <fglad@gmail.com>
2024-02-15 11:02:17 -0600

    Add image.

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -19,6 +19,7 @@
 <Base>
     <button class="gap-2 aspect-square w-48 border rounded-lg" style="background-color: {backgroundColor}; border-color: {borderColor};">
         <div class="p-2 h-full text-center flex items-center justify-center snap-start overflow-hidden flex-col text-base font-semibold">
+            <img class="w-full aspect-square" src={data.image} alt={data.name} />
             <p>{data.name}</p>
             <p>{data.data.subName}</p>
             <p class="text-sm font-normal">{data.data.description}</p>
