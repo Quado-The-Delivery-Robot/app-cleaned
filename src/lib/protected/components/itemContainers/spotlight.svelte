1f3489a72786fad337e0d2c624fa695aa3417cdd
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-02-16 21:26:52 -0600

    Improve layout.

--- src/lib/protected/components/itemContainers/spotlight.svelte
+++ src/lib/protected/components/itemContainers/spotlight.svelte
@@ -27,11 +27,11 @@
 
 <Base {data}>
     <div class="gap-2 aspect-square w-60 onlyGlass relative text-center font-semibold" style="background-color: rgba({hexToRgb(backgroundColor)}, var(--tw-bg-opacity)); border-color: {borderColor};">
-        <div class="w-full h-1/2 absolute left-1/2 -translate-x-1/2 bottom-1/2 flex justify-center items-center pb-1">
+        <div class="w-full h-2/3 absolute left-1/2 -translate-x-1/2 bottom-1/2 flex justify-center items-center pb-1">
             <img class="h-full aspect-square" src={data.image} alt={data.name} />
         </div>
 
-        <div class="w-full h-1/2 absolute left-1/2 -translate-x-1/2 top-1/2 mt-2 pt-1">
+        <div class="w-full h-1/3 absolute left-1/2 -translate-x-1/2 top-1/2 mt-2 pt-1 flex flex-col justify-center items-center">
             <p class="font-semibold">{data.name}</p>
             <p class="text-sm font-normal max-w-[80%] mt-2">{data.data.subName} | {data.data.description}</p>
         </div>
