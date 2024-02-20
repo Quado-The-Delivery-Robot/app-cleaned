85b3da83ce428e47fe02872350ef6e5198a6ef6a
Landon Redmond <name1234561.po@gmail.com>
2024-02-20 10:56:56 -0600

    Fix spelling.

--- src/routes/(app)/(protected)/cart/+page.svelte
+++ src/routes/(app)/(protected)/cart/+page.svelte
@@ -26,7 +26,7 @@
                 <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
             </svg>
 
-            <p class="text-xl">Try placing item in your cart!</p>
+            <p class="text-xl">Try placing items in your cart!</p>
         </div>
     {/if}
 {/await}
