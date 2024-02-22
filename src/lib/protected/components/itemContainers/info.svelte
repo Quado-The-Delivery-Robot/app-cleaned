bb38cec380813e4559438eebfbdea63626e6466c
kursten putman <fglad@gmail.com>
2024-02-22 10:38:41 -0600

    changed background and border color to rgb

--- src/lib/protected/components/itemContainers/info.svelte
+++ src/lib/protected/components/itemContainers/info.svelte
@@ -1,28 +1,22 @@
 <script lang="ts">
     import Base from "./base.svelte";
-    import shadeColor from "$lib/protected/shadeColor";
     import hexToRgb from "$lib/protected/hexToRGB";
     import type { sectionItem } from "$lib/core/types";
-    import { onMount } from "svelte";
 
     export let data: sectionItem;
-    let backgroundColor: string = "#000000";
-    let borderColor: string = "#000000";
-
-    onMount(() => {
-        if ("colors" in data) {
-            backgroundColor = (data as any).colors[0];
-            borderColor = shadeColor(backgroundColor, -50);
-        }
-    });
+    let backgroundColor: string;
+    let borderColor: string;
 </script>
 
-<Base {data}>
+<Base {data} bind:backgroundColor bind:borderColor>
     <div class=" w-32 aspect-square onlyGlass" style="background-color: rgba({hexToRgb(backgroundColor)}, var(--tw-bg-opacity)); border-color: {borderColor};">
-        <img class="w-full aspect-square p-2" src={data.image} alt={data.name} />
-    </div>
-    <div class="w-full justify-between items-center">
-        <p>{data.name}</p>
-        <p>{data.data.subName}</p>
+        <div class="w-full flex justify-center items-center">
+            <img class="w-full aspect-square" src={data.image} alt={data.name} />
+        </div>
+
+        <div class="w-full justify-between items-center">
+            <p>{data.name}</p>
+            <p>{data.data.subName}</p>
+        </div>
     </div>
 </Base>
