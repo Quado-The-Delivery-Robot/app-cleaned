bd811926d24cb8786da691a8beeff27e16c7ce9f
kursten putman <fglad@gmail.com>
2024-02-23 11:20:35 -0600

    chnaged to flex col

--- src/lib/protected/components/itemContainers/info.svelte
+++ src/lib/protected/components/itemContainers/info.svelte
@@ -10,7 +10,7 @@
 
 <Base {data} bind:backgroundColor bind:borderColor>
     <div class=" w-60 onlyGlass" style="background-color: rgba({hexToRgb(backgroundColor)}, var(--tw-bg-opacity)); border-color: {borderColor};">
-        <div class="w-full flex flex-row justify-center items-center gap-2">
+        <div class="w-full flex flex-col justify-center items-center gap-2">
             <img class="w-14 h-14 aspect-square" src={data.image} alt={data.name} />
             <p class="text-sm">{data.name}</p>
             <p class="text-sm ml-2">{data.data.subName} <span class="mr-2">{data.data.calories} calories</span></p>
