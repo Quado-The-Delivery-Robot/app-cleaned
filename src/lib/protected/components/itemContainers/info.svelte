28b3b3de4b720b83e8d6acfbb2eb3f8f0f6bb3e7
kursten putman <fglad@gmail.com>
2024-02-23 11:25:15 -0600

    removed flex col

--- src/lib/protected/components/itemContainers/info.svelte
+++ src/lib/protected/components/itemContainers/info.svelte
@@ -12,10 +12,8 @@
     <div class=" w-60 onlyGlass" style="background-color: rgba({hexToRgb(backgroundColor)}, var(--tw-bg-opacity)); border-color: {borderColor};">
         <div class="w-full flex justify-center items-center gap-2">
             <img class="w-14 h-14 aspect-square" src={data.image} alt={data.name} />
-            <div class="flex-col">
-                <p class="text-sm">{data.name}</p>
-                <p class="text-sm ml-2">{data.data.subName}|{data.data.calories} calories</p>
-            </div>
+            <p class="text-sm">{data.name}</p>
+            <p class="text-sm ml-2">{data.data.subName}|{data.data.calories} calories</p>
         </div>
     </div>
 </Base>
