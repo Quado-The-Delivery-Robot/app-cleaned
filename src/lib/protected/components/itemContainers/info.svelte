9139f152d28cb7f8a3aaf60055010f31b433cf67
kursten putman <fglad@gmail.com>
2024-02-23 11:23:15 -0600

    changed the div flex colum is in and added a line between price and calories

--- src/lib/protected/components/itemContainers/info.svelte
+++ src/lib/protected/components/itemContainers/info.svelte
@@ -10,10 +10,12 @@
 
 <Base {data} bind:backgroundColor bind:borderColor>
     <div class=" w-60 onlyGlass" style="background-color: rgba({hexToRgb(backgroundColor)}, var(--tw-bg-opacity)); border-color: {borderColor};">
-        <div class="w-full flex flex-col justify-center items-center gap-2">
+        <div class="w-full flex justify-center items-center gap-2">
             <img class="w-14 h-14 aspect-square" src={data.image} alt={data.name} />
-            <p class="text-sm">{data.name}</p>
-            <p class="text-sm ml-2">{data.data.subName} <span class="mr-2">{data.data.calories} calories</span></p>
+            <div class="flex-col">
+                <p class="text-sm">{data.name}</p>
+                <p class="text-sm ml-2">{data.data.subName}|{data.data.calories} calories</p>
+            </div>
         </div>
     </div>
 </Base>
