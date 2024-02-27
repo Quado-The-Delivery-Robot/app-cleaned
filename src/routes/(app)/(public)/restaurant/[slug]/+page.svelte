e7dac8e7e5f3093a497851d09913ae4b79839c11
Landon Redmond <name1234561.po@gmail.com>
2024-02-27 10:23:53 -0600

    Edit layout.

--- src/routes/(app)/(public)/restaurant/[slug]/+page.svelte
+++ src/routes/(app)/(public)/restaurant/[slug]/+page.svelte
@@ -20,15 +20,19 @@
         <p>Loading...</p>
     {:then}
         <div class="text-lg relative">
-            {#each Array(2) as _, index}
-                <div class="flex gap-0.5 {index == 1 ? 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-full' : ''}" style="width: {index == 1 ? (index / 5) * 100 : 100}%;">
-                    {#each Array(5) as _index}
-                        <svg class="w-6 aspect-square {index == 1 ? 'fill-yellow-500' : 'fill-white'}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
-                        </svg>
-                    {/each}
-                </div>
-            {/each}
+            <div class="w-full">
+                {#each Array(2) as _, index}
+                    <div>
+                        <div class="flex gap-0.5 {index == 1 ? 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-full' : ''}" style="width: {index == 1 ? (index / 5) * 100 : 100}%;">
+                            {#each Array(5) as _index}
+                                <svg class="w-6 aspect-square {index == 1 ? 'fill-yellow-500' : 'fill-white'}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                                </svg>
+                            {/each}
+                        </div>
+                    </div>
+                {/each}
+            </div>
 
             <p>4.9</p>
         </div>
