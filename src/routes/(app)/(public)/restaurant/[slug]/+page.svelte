c9ce4f5c46a8fb37b40618eccfa1989b487b2684
kursten putman <fglad@gmail.com>
2024-02-29 10:58:29 -0600

    changed decriptions margin top

--- src/routes/(app)/(public)/restaurant/[slug]/+page.svelte
+++ src/routes/(app)/(public)/restaurant/[slug]/+page.svelte
@@ -30,7 +30,7 @@
             <Rating rating={2.7} />
 
             <p class=" text-sm font-semibold mt-2">{data.restaurant.location}</p>
-            <p class="mt-2">{data.restaurant.description}</p>
+            <p class="mt-3">{data.restaurant.description}</p>
         </div>
         {#each Object.values(data.feed) as section}
             <Section name={section.name} hasHeader={section.hasHeader} items={section.data} sectionType={section.sectionType} itemType={section.itemType} />
