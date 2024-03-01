3f770ceba8f8bf028262b91b619ef45b9b4e6f15
Landon Redmond <landon.redmond0@gmail.com>
2024-03-01 11:11:37 -0600

    Improve styling.

--- src/lib/protected/components/itemContainers/info.svelte
+++ src/lib/protected/components/itemContainers/info.svelte
@@ -10,10 +10,10 @@
 
 <Base {data} bind:backgroundColor bind:borderColor>
     <div class="onlyGlass w-60 overflow-hidden" style="background-color: rgba({hexToRgb(backgroundColor)}, var(--tw-bg-opacity)); border-color: {borderColor};">
-        <div class="w-full flex justify-start items-center gap-2">
+        <div class="w-full flex justify-start items-center gap-2 overflow-hidden">
             <img class="w-14 h-14 aspect-square" src={data.image} alt={data.name} />
 
-            <div class="flex flex-col justify-start items-start w-full text-left [&>p]:overflow-hidden [&>p]:text-nowrap [&>p]:text-ellipsis">
+            <div class="flex flex-col justify-start items-start flex-grow text-left [&>p]:overflow-hidden [&>p]:text-nowrap [&>p]:text-ellipsis">
                 <p class="font-semibold">{data.name}</p>
                 <p class="text-sm">{data.data.subName} | {data.data.calories} calories</p>
             </div>
