ca6514f854e9ea88d57ca5d238a5a8f3fd43589a
kursten putman <fglad@gmail.com>
2024-03-06 11:20:25 -0600

    changed import from restaurant to sectionItem

--- src/lib/public/restaurant/menuSections.svelte
+++ src/lib/public/restaurant/menuSections.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-    import type { restaurant } from "$lib/core/types";
-    export let data: restaurant;
+    import type { sectionItem } from "$lib/core/types";
+    export let data: sectionItem;
 </script>
 
-<div class="lg:flex"></div>
+<div class="lg:flex">
+    {data.type}
+</div>
