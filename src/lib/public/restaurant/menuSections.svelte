b439cc9eb6c88019062a8d5eaffcc25fed7ce636
kursten putman <fglad@gmail.com>
2024-03-06 11:11:18 -0600

    removed  data.tag

--- src/lib/public/restaurant/menuSections.svelte
+++ src/lib/public/restaurant/menuSections.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import type { restaurant } from "$lib/core/types";
-
     export let data: restaurant;
 </script>
 
-<div class="lg:flex">
-    {data.tags}
-</div>
+<div class="lg:flex"></div>
