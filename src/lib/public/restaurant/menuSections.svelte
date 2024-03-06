00ac5bb85c2eb012264fb3c763d634c9014fdab8
kursten putman <fglad@gmail.com>
2024-03-06 11:08:55 -0600

    import restraurant into menuSection

--- src/lib/public/restaurant/menuSections.svelte
+++ src/lib/public/restaurant/menuSections.svelte
@@ -1,1 +1,9 @@
-<div class="lg:flex"></div>
+<script lang="ts">
+    import type { restaurant } from "$lib/core/types";
+
+    export let data: restaurant;
+</script>
+
+<div class="lg:flex">
+    {data.tags}
+</div>
