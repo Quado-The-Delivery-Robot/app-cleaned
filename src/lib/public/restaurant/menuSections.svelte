7e64a200697bca3838b9233f875ddde3664752e9
kursten putman <fglad@gmail.com>
2024-03-06 11:28:40 -0600

    removed imports in menuSection

--- src/lib/public/restaurant/menuSections.svelte
+++ src/lib/public/restaurant/menuSections.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-    import type { sectionItem } from "$lib/core/types";
-    export let data: sectionItem;
 </script>
 
-<div class="lg:flex">
-    {data.type}
-</div>
+<div class="lg:flex"></div>
