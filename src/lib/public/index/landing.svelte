291c8063eb5bce471e601ad2f47434b4822bc1b3
Landon Redmond <landon.redmond0@gmail.com>
2024-03-07 11:14:26 -0600

    Improve landing.

--- src/lib/public/index/landing.svelte
+++ src/lib/public/index/landing.svelte
@@ -1,6 +1,5 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center gap-6 z-[1]">
     <p class="text-[2.8rem] leading-[0.9] font-semibold"><span class="text-brand">Quado</span><br />delivery made easy</p>
-    <p class="text-lg text-primary-700 max-w-[80%] lg:max-w-[41%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sed, eum aliquid error assumenda optio illum distinctio at dignissimos!</p>
 
     <div class="flex gap-6 text-base">
         <a href="/login" class="glass">Register</a>
