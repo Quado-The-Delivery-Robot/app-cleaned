8d38c964a893d9b7ffcb64b9251d9441fc4f9eca
Landon Redmond <landon.redmond0@gmail.com>
2024-03-08 10:13:32 -0600

    Improve styling.

--- src/lib/public/index/landing.svelte
+++ src/lib/public/index/landing.svelte
@@ -1,7 +1,7 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center gap-6 z-[1]">
-    <p class="text-[2.8rem] leading-[0.9] font-semibold"><span class="text-brand">Quado</span><br />delivery made easy</p>
+    <p class="text-[2.8rem] leading-[0.9] font-semibold"><span class="text-brand font-bold">Quado</span><br />delivery made easy</p>
 
-    <div class="flex gap-6 text-base">
+    <div class="flex gap-6 text-base mt-2">
         <a href="/login" class="glass">Register</a>
         <button class="glass">Learn more</button>
     </div>
