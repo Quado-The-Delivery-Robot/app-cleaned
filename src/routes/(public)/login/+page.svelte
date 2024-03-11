5886b918fbe5b2c72b54bb3369d4a2af567081bb
Landon Redmond <landon.redmond0@gmail.com>
2024-03-11 11:20:52 -0500

    Add page title.

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -5,4 +5,8 @@
     export let data: PageData;
 </script>
 
+<svelte:head>
+    <title>{data.register ? "Register" : "Login"}</title>
+</svelte:head>
+
 <button class="absolute bottom-[100px] w-[55%] left-1/2 -translate-x-1/2 bg-brand rounded-xl px-12 py-4 text-2xl text-black max-w-[650px]" on:click={() => signIn("google")}>{data.register ? "Register" : "Login"}</button>
