37ca0de4f2fbb01b312eadd91ad5ee846baf311e
Landon Redmond <landon.redmond0@gmail.com>
2024-03-11 10:49:29 -0500

    Improve footer.

--- src/lib/public/index/footer.svelte
+++ src/lib/public/index/footer.svelte
@@ -1,10 +1,10 @@
-<footer class="w-full h-fit py-12 select-text flex flex-col items-center justify-center text-primary-700 text-base">
-    <div class="w-full pb-8 flex items-center justify-between px-[10%] 2xl:px-[18%]">
+<footer class="w-full h-fit py-12 select-text flex flex-col items-center justify-center text-primary-700 text-base [&>div]:px-[10%] 2xl:[&>div]:px-[18%]">
+    <div class="w-full pb-3 flex items-center justify-between flex-col md:flex-row">
         <a class="h-12 aspect-square" href="https://quadoapp.com">
             <img class="w-full h-full" src="/icon.png" alt="Quado logo" />
         </a>
 
-        <nav class="flex gap-8">
+        <nav class="flex gap-2 md:gap-8 flex-col md:flex-row">
             <a href="/">About</a>
             <a href="/privacy">Privacy Policy</a>
             <a href="/contact">Contact</a>
@@ -12,7 +12,7 @@
         </nav>
     </div>
 
-    <div class="w-full flex items-center justify-end px-[10%] 2xl:px-[18%]">
+    <div class="w-full flex items-center justify-start px-[10%] 2xl:px-[18%]">
         <p>©Copyright 2024, Quado.</p>
     </div>
 </footer>
