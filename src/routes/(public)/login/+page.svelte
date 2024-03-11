bb494e62ea7355952e190b15a378fda978f41dc4
Landon Redmond <landon.redmond0@gmail.com>
2024-03-11 11:30:08 -0500

    Add padding.

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -10,6 +10,6 @@
     <title>{data.register ? "Register" : "Login"}</title>
 </svelte:head>
 
-<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full"><Title /></div>
+<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-5"><Title /></div>
 
 <button class="absolute bottom-[100px] w-[55%] left-1/2 -translate-x-1/2 bg-brand rounded-xl px-12 py-4 text-2xl text-black max-w-[650px]" on:click={() => signIn("google")}>{data.register ? "Register" : "Login"}</button>
