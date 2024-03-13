c1742b6d86fbb08487c6f4bd6aa73a9f9849b5e1
Landon Redmond <landon.redmond0@gmail.com>
2024-03-13 10:14:05 -0500

    Decrease gap.

--- src/lib/protected/components/search.svelte
+++ src/lib/protected/components/search.svelte
@@ -25,7 +25,7 @@
     });
 </script>
 
-<div class="w-full flex flex-col gap-3 pb-6">
+<div class="w-full flex flex-col gap-2 pb-6">
     <div class="glass flex gap-3 rounded-full px-5 py-2.5">
         <button class="w-6 aspect-square" on:click={search}>
             <svg class="fill-white w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
