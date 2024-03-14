c1461c818553c2a51450c75ed24724f450bfd73b
Landon Redmond <landon.redmond0@gmail.com>
2024-03-14 10:42:35 -0500

    Fix database import.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "@auth/core/providers/google";
 import { GOOGLE_CLIENT_ID, GOOGLE_SECERT } from "$env/static/private";
-import database from "$lib/core/database";
+import database from "$lib/core/scripts/database";
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = SvelteKitAuth({
