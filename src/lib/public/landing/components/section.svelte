4c0805023c97ca852590448e7058d1d304de8645
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-15 16:09:14 -0500

    Improve landing page.

--- src/lib/public/landing/components/section.svelte
+++ src/lib/public/landing/components/section.svelte
@@ -7,13 +7,11 @@
 
 <span {id} class="relative"></span>
 
-<div class="flex flex-col justify-center md:justify-between items-center w-full h-screen px-10 {direction === 'right' ? 'md:flex-row-reverse' : 'md:flex-row'}" id="#{id}">
+<div class="flex flex-col justify-center md:justify-between items-center w-full h-screen px-10 relative {direction === 'right' ? 'md:flex-row-reverse' : 'md:flex-row'}" id="#{id}">
     <div class="w-full md:w-[45%] h-full text-center md:text-left flex flex-col justify-center items-center md:items-start gap-4 {direction === 'right' ? 'md:pr-[10%] 2xl:pr-[18%]' : 'md:pl-[10%] 2xl:pl-[18%]'}">
         <p class="text-3xl font-semibold">{@html title}</p>
         <p class="text-lg">{description}</p>
     </div>
 
-    <div class="w-full relative md:w-[55%] h-[80vh]">
-        <slot />
-    </div>
+    <slot />
 </div>
