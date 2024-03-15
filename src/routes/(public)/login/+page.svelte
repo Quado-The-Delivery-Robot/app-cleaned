d6e77ecdc88d46b8338b0710f8096ba84950aeb3
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-15 16:10:57 -0500

    Change login page background.

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -13,3 +13,9 @@
 <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-5"><BrandTitle /></div>
 
 <button class="absolute bottom-[100px] w-[55%] left-1/2 -translate-x-1/2 bg-brand rounded-xl px-12 py-4 text-2xl text-black max-w-[650px]" on:click={() => signIn("google")}>{data.register ? "Register" : "Login"}</button>
+
+<style lang="postcss">
+    :global(body) {
+        @apply bg-gradient-to-b from-backgroundSecondary to-background bg-no-repeat;
+    }
+</style>
