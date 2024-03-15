38e63851c47ea5bf8227f5f14a84debdb9b17d38
Landon Redmond <landon.redmond0@gmail.com>
2024-03-15 11:20:36 -0500

    Fix icon.

--- src/lib/public/landing/footer.svelte
+++ src/lib/public/landing/footer.svelte
@@ -1,7 +1,7 @@
 <footer class="w-full h-fit py-12 select-text flex flex-col items-center justify-center text-primary-700 text-base text-left [&>div]:px-[10%] 2xl:[&>div]:px-[18%]">
     <div class="w-full pb-5 md:pb-3 flex items-start md:items-center justify-start md:justify-between flex-col md:flex-row">
         <a class="h-12 aspect-square mb-5 md:mb-0" href="https://quadoapp.com">
-            <img class="w-full h-full" src="/images/icon.svg" alt="Quado logo" />
+            <img class="w-full h-full" src="/images/icon.png" alt="Quado logo" />
         </a>
 
         <nav class="flex gap-2 md:gap-8 flex-col md:flex-row">
