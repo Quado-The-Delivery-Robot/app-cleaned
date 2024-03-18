4d396d09eeb307169828018da8856cc9f36e41ea
Landon Redmond <landon.redmond0@gmail.com>
2024-03-18 11:12:58 -0500

    Improve item container.

--- src/lib/public/restaurant/items.svelte
+++ src/lib/public/restaurant/items.svelte
@@ -23,7 +23,7 @@
     <div class="flex gap-[1px] flex-row lg:flex-col overflow-hidden rounded-xl border border-background-800 bg-background-800">
         {#each Object.entries(groups) as [group, items]}
             <button
-                class="onlyGlass px-5 py-2.5"
+                class="onlyGlass px-6 py-3"
                 on:click={() => {
                     currentGroup = group;
                 }}
@@ -36,7 +36,22 @@
     <div class="flex-grow grid grid-cols-2 auto-rows-max">
         {#each items as item}
             {#if item.type === currentGroup || currentGroup === "All"}
-                <p>{item.name}</p>
+                <div class="onlyGlass w-full overflow-hidden">
+                    <div class="w-full flex justify-start items-center gap-2 overflow-hidden">
+                        <img class="w-14 h-14 aspect-square" src={item.image} alt={item.name} />
+
+                        <div class="flex flex-col justify-start items-start flex-grow text-left [&>p]:overflow-hidden [&>p]:text-nowrap [&>p]:text-ellipsis">
+                            <p class="text-lg font-semibold">{item.name}</p>
+
+                            <div class="flex gap-2">
+                                <p>${item.price}</p>
+                                <p>{item.calories} calories</p>
+                            </div>
+
+                            <p>{item.description}</p>
+                        </div>
+                    </div>
+                </div>
             {/if}
         {/each}
     </div>
