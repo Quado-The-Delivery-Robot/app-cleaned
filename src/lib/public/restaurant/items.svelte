70814577fff00a8b73f7cca819e27a771b9be901
Landon Redmond <landon.redmond0@gmail.com>
2024-03-19 10:25:20 -0500

    Remove item count and fix overflow.

--- src/lib/public/restaurant/items.svelte
+++ src/lib/public/restaurant/items.svelte
@@ -20,16 +20,14 @@
 </script>
 
 <div class="w-full flex flex-col lg:flex-row gap-4">
-    <div class="flex gap-2 flex-row lg:flex-col w-full md:w-[20%] lg:w-[13%] h-fit overflow-x-scroll">
-        {#each Object.entries(groups) as [group, items]}
+    <div class="flex gap-2 flex-row lg:flex-col w-full md:min-w-[20%] lg:min-w-[13%] h-fit overflow-x-auto">
+        {#each Object.keys(groups) as group}
             <button
                 class="glass w-full px-6 py-3 text-nowrap text-left"
                 on:click={() => {
                     currentGroup = group;
-                }}
+                }}>{group}</button
             >
-                {group} <span class="text-primary-700">({items.length})</span>
-            </button>
         {/each}
     </div>
 
