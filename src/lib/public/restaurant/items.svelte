adbc7778d56d48ac296e08f7132eff2674c6c70e
Landon Redmond <landon.redmond0@gmail.com>
2024-03-20 10:17:16 -0500

    Improve mobile styling.

--- src/lib/public/restaurant/items.svelte
+++ src/lib/public/restaurant/items.svelte
@@ -23,7 +23,7 @@
     <div class="flex gap-2 flex-row lg:flex-col w-full md:min-w-[188px] md:w-[188px] h-fit overflow-x-auto">
         {#each Object.keys(groups) as group}
             <button
-                class="w-fit md:w-full px-3 py-3 text-nowrap text-left hover:bg-secondary border-opacity-0 hover:border-opacity-100 border border-secondary-300 transition-all rounded-lg"
+                class="w-fit md:w-full px-4 md:px-3 py-2 md:py-3 text-nowrap text-left hover:bg-secondary border-opacity-0 hover:border-opacity-100 border border-secondary-300 transition-all rounded-lg"
                 on:click={() => {
                     currentGroup = group;
                 }}>{group}</button
@@ -35,7 +35,7 @@
         {#each items as item}
             {#if item.type === currentGroup || currentGroup === "All"}
                 <div class="primary w-full overflow-hidden rounded-lg">
-                    <div class="w-full flex justify-start items-center gap-2 overflow-hidden pr-6 h-full">
+                    <div class="w-full flex flex-col md:flex-row justify-start items-center gap-2 overflow-hidden pr-6 h-full">
                         <div class="flex flex-col justify-start items-start flex-grow text-left px-6 py-6 h-full">
                             <p class="text-lg font-semibold leading-none">{item.name}</p>
 
@@ -47,7 +47,7 @@
                             <p class="mt-3 overflow-hidden line-clamp-3 overflow-ellipsis">{item.description}</p>
                         </div>
 
-                        <img class="w-36 aspect-square" src={item.image} alt={item.name} />
+                        <img class="w-28 md:w-36 aspect-square" src={item.image} alt={item.name} />
                     </div>
                 </div>
             {/if}
