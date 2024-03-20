6af29ee2d1141364545a810ff3dd296d7dadac0a
Landon Redmond <landon.redmond0@gmail.com>
2024-03-20 10:37:23 -0500

    Fix version.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import * as child_process from "node:child_process";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        version: 0.1,
+        version: {
+            name: child_process.execSync("git rev-parse HEAD").toString().trim(),
+        },
         adapter: adapter(),
         csrf: {
             checkOrigin: false,
