ec997dffda5df4e44b08871660bade07fd117572
Landon Redmond <landon.redmond0@gmail.com>
2024-04-18 10:26:15 -0500

    Improve and fix link arrow.

--- src/lib/protected/components/section.svelte
+++ src/lib/protected/components/section.svelte
@@ -36,8 +36,8 @@
         <div class="flex justify-between items-center mb-1">
             <p class="text-lg font-semibold">{name}</p>
             <a href="/{name.toLowerCase()}/all" class="flex items-center text-sm opacity-50 font-light"
-                >See all <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="aspect-square h-6 fill-white">
-                    <path fill-rule="evenodd" d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z" clip-rule="evenodd" />
+                >See all <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="aspect-square h-4 fill-white">
+                    <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
                 </svg>
             </a>
         </div>
